83652f0ece6d64d1540e2836e5122ff2db680183
David Gómez Sedas <david@MacBook-Air-de-David.local>
2024-11-02 01:06:38 +0000

    day one complete

---
+++ day_1/helloworld.py
@@ -0,0 +1,60 @@
+# Open the python interactive shell and do the following operations. The operands are 3 and 4.
+a = 3
+b = 4
+
+print(a * b)
+print(a - b)
+print(a + b)
+print(a % b)
+print(a / b)
+print(a ** b)
+print(a // b)
+
+
+# Write strings on the python interactive shell. The strings are the following:
+#Your name
+#Your family name
+#Your country
+#I am enjoying 30 days of python
+name = "David"
+family_name = "Gómez"
+country = "Spain"
+phrase = "I am enjoying 30 days of python"
+
+# Check the data types of the following data:
+# 10
+# 9.8
+# 3.14
+# 4 - 4j
+# ['Asabeneh', 'Python', 'Finland']
+# Your name
+# Your family name
+# Your country 
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(family_name))
+print(type(country))
+
+# Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+ex_int = 1
+ex_float = 1.1
+ex_complex = 1j
+ex_string = "hello"
+ex_boolean = True
+ex_list = [1, 2, "three"]
+ex_tuple = (1, 2, "three")
+ex_set = set(1, 5, 9)
+ex_dictionary = {1: "one", 2:"two"}
+
+
+
+
+
+
+
+
